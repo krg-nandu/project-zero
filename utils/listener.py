8e9e469432f8278db472f05285a5921a44aaeae1
Lakshmi Narasimhan <krg.lakshminarasimhan@gmail.com>
2019-03-18 21:47:52 +0000

    adding gitignore, minor change to text parsing

--- utils/listener.py
+++ utils/listener.py
@@ -7,17 +7,17 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(BUTTON, GPIO.IN)
 
 def notify_button_press():
-	os.system("espeak -ven+f5 -s190 'Go Ahead'")
+	os.system("espeak -ven+f5 -s170 'Go Ahead'")
 
 def collect_and_process_audio():
 	os.system("bash get_and_parse_input.sh")
 	return 'tmpf.txt'
 
 def voice_error_message():
-	os.system("espeak -ven+f5 -s190 'I did not understand that'")
+	os.system("espeak -ven+f5 -s170 'I did not understand that'")
 
 def voice_introduce():
-	os.system("espeak -ven+f5 -s190 'My name is zero \n I am still in beta mode'")
+	os.system("espeak -ven+f5 -s170 'My name is zero. I am still in beta mode'")
 def main():
 	# passively wait for the hardware trigger
 	while True:
@@ -26,12 +26,12 @@ def main():
 		# on button press
 		if not state:
 			# first signal that Zero is ready to listen
-			#notify_button_press()
+			notify_button_press()
 			# collect audio input from the microphone
-			#outputf = collect_and_process_audio()
-			outputf = 'output2.txt'
+			outputf = collect_and_process_audio()
+			#outputf = 'output2.txt'
 			#import ipdb; ipdb.set_trace();
-			stri = open(outputf,'r').read()[1:-2]
+			stri = open(outputf,'r').read().strip('\n\"\'')
 			if stri == "introduce yourself":
 				voice_introduce()
 			else:
